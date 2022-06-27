8329e5bf34480030edd65d0c5aa8d0330d91b48a
Martynas <Martynas@DESKTOP-HTUUCFV>
2022-06-27 18:00:43 +0300

    Add project files.

---
+++ Framework/Driver.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Framework
+{
+    public class Driver
+    {
+        private static IWebDriver driver;
+
+        public static void setDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static IWebDriver getDriver()
+        {
+            return driver;
+        }
+
+        public static void open(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static void closeDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Tests/DemoTest.cs
@@ -0,0 +1,16 @@
+﻿using Framework;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DemoTest
+    {
+        [Test]
+        public void openGoogle()
+        {
+            Driver.setDriver();
+            Driver.open("https://www.google.com");
+            Driver.closeDriver();
+        }
+    }
+}
